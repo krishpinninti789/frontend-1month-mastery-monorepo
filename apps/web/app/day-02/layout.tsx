0ef6da0c8b9595c7bb05ed2a1912c57e429a6143
Krishnavamsi P <krishnavamsi.p@MB-M-KRISHNAVMSI.local>
2026-03-21 19:15:11 +0530

    meat content

--- apps/web/app/day-02/layout.tsx
+++ apps/web/app/day-02/layout.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import type { Metadata } from "next";
 import { generateMetaData } from "@repo/lib";
 
 export const metadata: Metadata = generateMetaData({
-  title: "Home",
-  description: "Welcome",
+  title: "Home and on day 2",
+  description: "Welcome to day2",
 });
 
-const layout = () => {
-  return <div></div>;
+const layout = ({ children }: { children: ReactNode }) => {
+  return <div>{children}</div>;
 };
 
 export default layout;
