79072eef03b903dfeb7f18a90c0f1f6460c86585
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2026-04-02 22:47:27 +0530

    day 4 worked on calculator project

--- apps/web/app/day-04/page.tsx
+++ apps/web/app/day-04/page.tsx
@@ -2,21 +2,66 @@
 
 import { useState } from "react";
 
+const options = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  "+",
+  "-",
+  "*",
+  "/",
+  "%",
+  "^",
+  "=",
+  "C",
+];
+
 export default function DAY04() {
-  const [state, setState] = useState("");
+  const [exp, setExp] = useState("");
+  const [res, setRes] = useState("");
+
+  const handleAdd = (item) => {
+    if (item == "C") {
+      setExp("");
+      setRes("");
+      return;
+    }
+    if (item === "=") {
+      if (!/^[0-9+\-*/.() ]+$/.test(exp)) {
+        throw new Error("Invalid input");
+      }
+      const result = eval(exp);
+      setRes(result);
+      return;
+    }
+
+    setExp((prev) => prev + item);
+  };
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1>DAY-04</h1>
-      <p>Build your daily project here</p>
-
-      <input
-        value={state}
-        onChange={(e) => setState(e.target.value)}
-        placeholder="Type something"
-      />
-
-      <p>{state}</p>
+    <div className="p-4 flex flex-col justify-center items-center h-screen border border-gray-600">
+      <div className="border border-gray-500">
+        <div className="h-20 bg-green-200 text-black">{exp}</div>
+        <div>Result: {res}</div>
+        <div className="grid grid-cols-4 border-2 ">
+          {options.map((item, idx) => (
+            <div
+              key={idx}
+              className="p-6 border-2 border-gray-500 cursor-pointer"
+              onClick={() => handleAdd(item)}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
